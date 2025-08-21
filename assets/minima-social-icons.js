fe96b39fd337336ef0a329366ee2cc7e8dcd3ef8
Sebastian <sdelapena@ronival.com>
2025-08-21 01:57:46 -0700

    fix nav bar

--- assets/minima-social-icons.js
+++ assets/minima-social-icons.js
@@ -5,14 +5,5 @@ document.addEventListener('DOMContentLoaded', () => {
     checkbox.addEventListener('change', () => {
       nav.style.display = checkbox.checked ? 'block' : 'none';
     });
-    // This is for cases where the URL changes but the page doesn't reload
-    // e.g., using Jekyll's default `relative_url` filter on anchors
-    // This makes sure the menu is closed when navigating
-    document.querySelectorAll('a').forEach(link => {
-      link.addEventListener('click', () => {
-        checkbox.checked = false;
-        nav.style.display = 'none';
-      });
-    });
   }
 });
